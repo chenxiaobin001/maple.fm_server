2d51b51f87a154244b441551635c51cae6585b09
eureka <lakayiiii@gmail.com>
2015-03-09 13:53:40 -0400

    headers

--- config/initializers/simple_token_authentication.rb
+++ config/initializers/simple_token_authentication.rb
@@ -35,7 +35,7 @@ SimpleTokenAuthentication.configure do |config|
     #   Then both the header names identifier key and default value are modified accordingly:
     #     `config.header_names = { super_admin: { phone_number: 'X-SuperAdmin-PhoneNumber' } }`
     #
-    #config.header_names = { user: { authentication_token: 'X-User-Token', email: 'X-User-Email' } }
+    config.header_names = { user: { authentication_token: 'X-User-Token', email: 'X-User-Email' } }
 
     # Configure the name of the attribute used to identify the user for authentication.
     # That attribute must exist in your model.
